b1c4b540317aab8c86717d8a2ccb48b4ecd7fa6f
Wei <jrweizhang@gmail.com>
2013-03-24 17:19:44 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# -*- encoding : utf-8 -*-
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listen::Application.config.secret_token = '2189d14c0549e7d10951ca6f4b9a6139b903f99965b42a9f7d42a68c17985f168d80bba6de861a0978f2a146a0523af9621f87b383ae40027a64e76027692c85'
