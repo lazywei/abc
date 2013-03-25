a6f5a952d5d451478002ef0858129b2bddabbb4a
Wei <jrweizhang@gmail.com>
2013-03-25 22:45:45 +0800

    Intro added.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,36 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require intro
 //= require_tree .
+
+
+$('#intro-btn').bind('click', function() {
+  $('#playerBox').css('position', 'absolute');
+  intro = introJs()
+  intro.oncomplete(function() {
+    $('#playerBox').css('position', 'fixed');
+  });
+
+  intro.onexit(function() {
+    $('#playerBox').css('position', 'fixed');
+  });
+  intro.start();
+});
+
+
+$('.topics h4:eq(0)').attr('data-step','0');
+$('.topics h4:eq(0)').attr('data-intro','Click a topic.');
+
+$('.a-time').attr('data-step','1');
+$('.a-time').attr('data-intro','Play the audio <br />and setting <br />A-time.');
+
+$('.b-time').attr('data-step','2');
+$('.b-time').attr('data-intro','Set B-time.');
+
+$('.store').attr('data-step','3');
+$('.store').attr('data-intro','Store the interval.');
+
+$('.clear').attr('data-step','4');
+$('.clear').attr('data-intro','Click Clear to Exit repeat mode.');
