689dd6356b35af64bd9e0e183b3a85399f8a1532
Wei <jrweizhang@gmail.com>
2013-03-25 23:16:04 +0800

    Chinese intro added.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,17 +31,20 @@ $('#intro-btn').bind('click', function() {
 });
 
 
-$('.topics h4:eq(0)').attr('data-step','0');
-$('.topics h4:eq(0)').attr('data-intro','Click a topic.');
+$('ul.topics').attr('data-step','0');
+$('ul.topics').attr('data-intro','This is a Site for listen 60 sec science with AB-Repeator.<br />這是一個可以使用 AB-Repeat （反覆聽同一句）聽取 60 秒科學的網站！');
 
-$('.a-time').attr('data-step','1');
-$('.a-time').attr('data-intro','Play the audio <br />and setting <br />A-time.');
+$('.topics h4:eq(0)').attr('data-step','1');
+$('.topics h4:eq(0)').attr('data-intro','Click a topic.<br />點選一個主題');
 
-$('.b-time').attr('data-step','2');
-$('.b-time').attr('data-intro','Set B-time.');
+$('.a-time').attr('data-step','2');
+$('.a-time').attr('data-intro','Play the audio <br />and setting <br />A-time.<br /><br />點 Set A 開始播放並且設定 A 點時間');
 
-$('.store').attr('data-step','3');
-$('.store').attr('data-intro','Store the interval.');
+$('.b-time').attr('data-step','3');
+$('.b-time').attr('data-intro','Wait and set B-time.<br /><br />接著設定 B 點時間');
 
-$('.clear').attr('data-step','4');
-$('.clear').attr('data-intro','Click Clear to Exit repeat mode.');
+$('.store').attr('data-step','4');
+$('.store').attr('data-intro','Store the interval.<br /><br />儲存當下設定好的反覆區間');
+
+$('.clear').attr('data-step','5');
+$('.clear').attr('data-intro','Click Clear to Exit repeat mode.<br /><br />離開重播模式');
